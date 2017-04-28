01363e7ab54e20a13e65f7ab789bd332f65c204e
Timur Kiyivinski <deathseclipse@gmail.com>
2017-04-28 19:02:22 +0800

    [DONE] SanBot

--- src/TelegramSanBot.cr
+++ src/TelegramSanBot.cr
@@ -1,5 +1,37 @@
 require "./TelegramSanBot/*"
+require "TelegramBot"
 
 module TelegramSanBot
-  # TODO Put your code here
+    class SanBot < TelegramBot::Bot
+        include TelegramBot::CmdHandler
+
+        def san(input : String, separator : String)
+            san = String.new
+            input.each_char do |char|
+                if char != 'e' && char != 'E'
+                    san += "#{char}#{separator}"
+                end
+            end
+            san
+        end
+
+        def initialize(@token)
+            super("SanBot", @token)
+
+            cmd "san" do |msg, params|
+                sanned = String.new
+                params.each { |word| sanned += "#{san(word, "")} " }
+                reply msg, sanned
+            end
+
+            cmd "sans" do |msg, params|
+                sanned = String.new
+                params.each { |word| sanned += "#{san(word, " ")} " }
+                reply msg, sanned
+            end
+        end
+    end
+
+    san_bot = SanBot.new ENV["SAN_TELEGRAM_TOKEN"]
+    san_bot.polling
 end
